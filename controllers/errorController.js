69bdd7a48cf38972cd787703c75f9c9ddbf2dafb
Minh Durbin <minhd12@gmail.com>
2021-03-31 17:03:27 -0600

    add username to home.ejs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,12 +10,12 @@ exports.logErrors = (error, req, res, next) => {
 exports.pageNotFound = (req, res) => {
   let errorCode = httpStatus.NOT_FOUND;
   res.status(errorCode);
-  res.render("error", {errorCode: errorCode});
+  res.render("error", {errorCode: errorCode });
 };
 //500 error
 exports.serverError = (error, req, res, next) => {
   let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
   res.status(errorCode);
-  res.render("error", {errorCode: errorCode});
+  res.render("error", {errorCode: errorCode });
   console.log(error.stack);
 };
