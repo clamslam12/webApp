86326ca5c3f07c9087162a5533b46077f96843b5
Minh Durbin <minhd12@gmail.com>
2021-04-15 20:23:45 -0600

    change User.create to User.register to create hash in seed.js

--- seed.js
+++ seed.js
@@ -3,7 +3,8 @@
 const { Promise } = require("bluebird");
 const mongoose = require("mongoose"),
   User = require("./models/user"),
-  Post = require("./models/post");
+  Post = require("./models/post"),
+  passport = require('passport');
 
 //initiate connection
 mongoose.connect("mongodb://localhost:27017/iMedia", {
@@ -11,89 +12,84 @@ mongoose.connect("mongodb://localhost:27017/iMedia", {
 });
 mongoose.connection;
 
-let posts = [{
-  post: "This is test post",
-  userEmail: "jon@wexler.com",
-  userName: "jwexler",
-  fullName: "test"
-},{
-  post: "A new post",
-  userEmail: "charlie@angel.com",
-  userName: "cangel", 
-  fullName: "test"
-},
-{
-  post: "A new post",
-  userEmail: "charlie@angel.com",
-  userName: "cangel", 
-  fullName: "test"
-
-},{
-  post: "A new post",
-  userEmail: "charlie@angel.com",
-  userName: "cangel", 
-  fullName: "test"
-
-},{
-  post: "A new post",
-  userEmail: "charlie@angel.com",
-  userName: "cangel", 
-  fullName: "test"
-
-},{
-  post: "A new post",
-  userEmail: "charlie@angel.com",
-  userName: "cangel", 
-  fullName: "test"
-
-},{
-  post: "A new post",
-  userEmail: "charlie@angel.com",
-  userName: "cangel", 
-  fullName: "test"
-
-}
-]
+let posts = [
+  {
+    post: "This is test post",
+    userEmail: "jon@wexler.com",
+    userName: "jwexler",
+    fullName: "test",
+  },
+  {
+    post: "A new post",
+    userEmail: "charlie@angel.com",
+    userName: "cangel",
+    fullName: "test",
+  },
+  {
+    post: "A new post",
+    userEmail: "charlie@angel.com",
+    userName: "cangel",
+    fullName: "test",
+  },
+  {
+    post: "A new post",
+    userEmail: "charlie@angel.com",
+    userName: "cangel",
+    fullName: "test",
+  },
+  {
+    post: "A new post",
+    userEmail: "charlie@angel.com",
+    userName: "cangel",
+    fullName: "test",
+  },
+  {
+    post: "A new post",
+    userEmail: "charlie@angel.com",
+    userName: "cangel",
+    fullName: "test",
+  },
+  {
+    post: "A new post",
+    userEmail: "charlie@angel.com",
+    userName: "cangel",
+    fullName: "test",
+  },
+];
 
 let users = [
   {
-    firstName: "Jon",
-    lastName: "Wexler",
+    name: { firstName: "Jon", lastName: "Wexler" },
     location: "Seattle",
     gender: "male",
     dateOfBirth: "1970-03-01",
     email: "jon@wexler.com",
     userName: "jwexler",
     password: "Jon12",
-    confirmPassword: "Jon12",
     securityQuestion: "nickName",
     securityResponse: "jonw",
     bio: "Hello this is jon wexler bio",
   },
   {
-    firstName: "Charlie",
-    lastName: "Angel",
+    name: { firstName: "Charlie", lastName: "Angel" },
     location: "Los Angeles",
     gender: "female",
     dateOfBirth: "1999-09-23",
     email: "charlie@angel.com",
     userName: "cangel",
     password: "Charlie23",
-    confirmPassword: "Charlie23",
     securityQuestion: "motherMaiden",
     securityResponse: "mom",
     bio: "Hello this is charlie angel bio",
   },
   {
-    firstName: "Elvis",
-    lastName: "John",
+    name: { firstName: "Elvis", lastName: "John" },
     location: "Austin",
     gender: "male",
     dateOfBirth: "2020-12-29",
     email: "elvis@john.com",
     userName: "ejohn",
     password: "Elvis29",
-    confirmPassword: "Elvis29",
     securityQuestion: "mascot",
     securityResponse: "tigers",
     bio: "Hello this is elvis john bio",
@@ -111,39 +107,41 @@ let commands = [];
 //for each user, push User.create commands in commands array
 //User.create is async
 users.forEach((u) => {
+  let newUser = new User(u);
   commands.push(
-    User.create({
-      firstName: u.firstName,
-      lastName: u.lastName,
-      location: u.location,
-      gender: u.gender,
-      dateOfBirth: u.dateOfBirth,
-      email: u.email,
-      userName: u.userName,
-      password: u.password,
-      confirmPassword: u.confirmPassword,
-      securityQuestion: u.securityQuestion,
-      securityResponse: u.securityResponse,
-      bio: u.bio,
-    })
+    // User.create({
+    //   firstName: u.firstName,
+    //   lastName: u.lastName,
+    //   location: u.location,
+    //   gender: u.gender,
+    //   dateOfBirth: u.dateOfBirth,
+    //   email: u.email,
+    //   userName: u.userName,
+    //   password: u.password,
+    //   confirmPassword: u.confirmPassword,
+    //   securityQuestion: u.securityQuestion,
+    //   securityResponse: u.securityResponse,
+    //   bio: u.bio,
+    // })
+    User.register(newUser, u.password)
   );
 });
 
 posts.forEach((post) => {
   commands.push(
-  Post.create({
-    post: post.post,
-    userEmail: post.userEmail,
-    userName: post.userName,
-    fullName: post.fullName
-  })
+    Post.create({
+      post: post.post,
+      userEmail: post.userEmail,
+      userName: post.userName,
+      fullName: post.fullName,
+    })
   );
 });
 
 //wait for all promises(async code) to resolve
 Promise.all(commands)
   .then((r) => {
-    console.log(r);//prints array of User objects
+    console.log(r); //prints array of User objects
   })
   .catch((error) => {
     console.log(`Error: ${error}`);
